a2017d5e5ae396f572615a59cbb5efc086e49649
Kirill Lukin <kirix.kl@gmail.com>
2018-04-14 23:16:50 +0300

    7 work

--- FHT/FHT_work.ino
+++ FHT/FHT_work.ino
@@ -9,8 +9,6 @@
 byte posOffset[33]={2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 25, 32, 36, 40, 44, 48, 52, 57, 62, 67, 72, 78, 84, 90, 96, 102, 108, 120};
 Servo Ser;
 void setup() {
-//  Ser.write(90);
-//  delay(5000);
   sbi(ADCSRA, ADPS2);
   cbi(ADCSRA, ADPS1);
   sbi(ADCSRA, ADPS0);
@@ -19,7 +17,6 @@ void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
-//int tmp[3];
 int ang=0;
 
 void loop() {
@@ -40,16 +37,14 @@ void loop() {
     fht_run();     // process the data in the fht
     fht_mag_log(); // take the output of the fht
     
-//    for (int i = 0; i < 128; i ++) {
     for (int i = 100; i < 128; i ++) {
         if (fht_log_out[i] < LOW_PASS) fht_log_out[i] = 0;
         fht_log_out[i] = (float)fht_log_out[i] * INPUT_GAIN;
     }
-//    for (byte pos = 0; pos < 32; pos++) {
       byte pos = 31;
       int posLevel = fht_log_out[posOffset[pos]];
       byte linesBetween;
-//      if (pos > 0 && pos < 32) {
+      
       if(pos > 0){
         linesBetween = posOffset[pos] - posOffset[pos - 1];
         for (byte i = 0; i < linesBetween; i++) {
@@ -69,50 +64,23 @@ void loop() {
         digitalWrite(13, 0);
       Serial.print(" ");
       if(!a)
-//        tmp[aa]=posLevel;
         ang=posLevel;
       else{
-//        tmp[aa]-=posLevel/2;
-//        ++aa;
         ang-=posLevel/2;
-        Serial.println("");
-        Serial.print(ang);
-        Serial.print("->");
-
         if(ang>=0){
-          ang = constrain(ang, 0, 400);
-          Serial.print(ang);
+          ang=constrain(ang, 0, 400);
           ang=map(ang, 0, 400, 90, 0);
         }
         if(ang<0){
           ang=-ang;
-          ang = constrain(ang, 0, 200);
-          Serial.print(ang);
+          ang=constrain(ang, 0, 200);
           ang=map(ang, 0, 200, 91, 180);
         }
-        Serial.print("=ang=");
+        Serial.print("ang=");
         Ser.write(ang);
         Serial.print(ang);
         Serial.println("");
       }
-/*      if(aa>=3){
-        aa=0;
-        for(int i=1; i<3;++i)
-          tmp[0]+=tmp[i];
-        Serial.println("");
-        Serial.print("ang=");
-        tmp[0]/=3;
-        if(tmp[0]<0){
-          tmp[0]*=-1;
-          tmp[0]=map(posLevel, 0, 200, 91, 180);
-        }
-        if(tmp[0]>=0){
-          tmp[0]=map(posLevel, 0, 200, 0, 90);
-        }
-        Ser.write(tmp[0]);
-        Serial.print(tmp[0]);
-        Serial.println("");
-      }*/
   }
   Serial.println("");
   delay(200);
